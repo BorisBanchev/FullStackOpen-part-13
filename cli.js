b6c1917c4f08c41ebcb8fcb9ee2b5abcaa9fc466
BorisBanchev <boris.banchev03@gmail.com>
2025-04-15 14:49:04 +0300

    Add exercise 13.4

--- cli.js
+++ cli.js
@@ -1,46 +1,11 @@
-require("dotenv").config();
-const { Sequelize, Model, DataTypes } = require("sequelize");
-const sequelize = new Sequelize(process.env.DATABASE_URL);
-
-class Blog extends Model {}
-
-Blog.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    author: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-    },
-    url: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    title: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    likes: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0,
-    },
-  },
-  {
-    sequelize,
-    underscored: true,
-    timestamps: false,
-    modelName: "blog",
-  }
-);
+import sequelize from "./connection_db.js";
+import Blog from "./models/Blog.js";
 
 const main = async () => {
   try {
     await sequelize.authenticate();
     console.log("Connection has been established successfully.");
-
+    await Blog.sync();
     const blogs = await Blog.findAll();
     blogs.forEach((blog) => {
       console.log(
--- index.js
+++ index.js
@@ -1,16 +1,55 @@
-require("dotenv").config();
-const { Sequelize } = require("sequelize");
+import sequelize from "./connection_db.js";
+import "dotenv/config";
+import express from "express";
+import Blog from "./models/Blog.js";
+const app = express();
+app.use(express.json());
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+app.get("/");
 
 const main = async () => {
   try {
     await sequelize.authenticate();
     console.log("Connection has been established successfully.");
-    sequelize.close();
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
 };
 
 main();
+
+app.get("/api/blogs/", async (req, res) => {
+  try {
+    const blogs = await Blog.findAll();
+    res.json(blogs);
+  } catch (error) {
+    console.error("Error fetching blogs:", error);
+    res.status(404).end();
+  }
+});
+
+app.post("/api/blogs", async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body);
+    console.log(blog.toJSON());
+    res.json(blog);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+app.delete("/api/blogs/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const deleted = await Blog.destroy({
+      where: { id },
+    });
+    res.json(deleted);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
