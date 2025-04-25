bebcd7720e763a8d3eb40681cda5a43661156d90
BorisBanchev <boris.banchev03@gmail.com>
2025-04-25 14:34:53 +0300

    Add exercise 13.15

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -51,6 +51,7 @@ blogsRouter.get("/", async (req, res) => {
         attributes: ["name"],
       },
       where,
+      order: [["likes", "DESC"]],
     });
     res.json(blogs);
   } catch (error) {
